49955559968a80fba005d40bb9e6f70e8a590304
Bilal Jalouk <bilaljalook@hotmail.com>
2025-03-07 09:20:37 +0100

    app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+import datetime
 
 
 '''
@@ -47,3 +49,31 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
 ## Finally, we can display the prediction to the user
 '''
+# Input controllers
+date = st.date_input("Date", datetime.date.today())
+time = st.time_input("Time", datetime.datetime.now().time())
+pickup_longitude = st.number_input("Pickup Longitude", value=0.0)
+pickup_latitude = st.number_input("Pickup Latitude", value=0.0)
+dropoff_longitude = st.number_input("Dropoff Longitude", value=0.0)
+dropoff_latitude = st.number_input("Dropoff Latitude", value=0.0)
+passenger_count = st.number_input("Passenger Count", min_value=1, max_value=8, value=1)
+
+# Convert date and time to string
+pickup_datetime = f"{date} {time}"
+
+# Build dictionary for API request
+params = {
+    "pickup_datetime": pickup_datetime,
+    "pickup_longitude": pickup_longitude,
+    "pickup_latitude": pickup_latitude,
+    "dropoff_longitude": dropoff_longitude,
+    "dropoff_latitude": dropoff_latitude,
+    "passenger_count": passenger_count
+}
+
+# Call API
+response = requests.get(url, params=params)
+prediction = response.json()
+
+# Display prediction
+st.write(f"Predicted fare: ${prediction['fare']:.2f}")
