eb90a4af82a0e85e3ae3edccb4f1cde0b54e0972
Bilal Jalouk <bilaljalook@hotmail.com>
2025-03-07 12:48:45 +0100

    c

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ import datetime
 '''
 # TaxiFareModel front
 '''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
 '''
 ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
@@ -33,23 +26,13 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 '''
 
-url = 'https://taxifare.lewagon.ai/predict'
+url = 'https://taxifare-662932307813.europe-west1.run.app/predict'
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
 
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
-# Input controllers
 date = st.date_input("Date", datetime.date.today())
 time = st.time_input("Time", datetime.datetime.now().time())
 pickup_longitude = st.number_input("Pickup Longitude", value=0.0)
@@ -58,10 +41,9 @@ dropoff_longitude = st.number_input("Dropoff Longitude", value=0.0)
 dropoff_latitude = st.number_input("Dropoff Latitude", value=0.0)
 passenger_count = st.number_input("Passenger Count", min_value=1, max_value=8, value=1)
 
-# Convert date and time to string
+
 pickup_datetime = f"{date} {time}"
 
-# Build dictionary for API request
 params = {
     "pickup_datetime": pickup_datetime,
     "pickup_longitude": pickup_longitude,
@@ -71,9 +53,7 @@ params = {
     "passenger_count": passenger_count
 }
 
-# Call API
 response = requests.get(url, params=params)
 prediction = response.json()
 
-# Display prediction
 st.write(f"Predicted fare: ${prediction['fare']:.2f}")
